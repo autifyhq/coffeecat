4bec557b064f6cf58eb6619baa200da88ff43dea
Takuya Suemura <takuyasuemura@gmail.com>
2020-09-05 19:58:12 +0900

    getToken

---
+++ src/getToken.js
@@ -0,0 +1,3 @@
+require("dotenv").config();
+
+module.exports = () => process.env.SLACK_BOT_TOKEN
--- src/initializeApp.js
+++ src/initializeApp.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 module.exports = () => (
   new App({
     token: process.env.SLACK_BOT_TOKEN,
